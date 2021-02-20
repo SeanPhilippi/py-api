ba31659a6966a3058799b53ff62223652a8723cd
SeanPhilippi <sean.philippi@gmail.com>
2021-02-20 13:51:50 -0600

    reworked, new plan, mostly working

--- py-api.py
+++ py-api.py
@@ -1,6 +1,5 @@
 import requests
 import json
-from difflib import SequenceMatcher
 import operator
 
 # 1. Removing irrelevant results
@@ -11,30 +10,31 @@ import operator
 #   - Email matches take precedent over name matches, except for the rule
 # above.
 
-def similar(result, term):
-    return SequenceMatcher(None, result, term).ratio()
+# check for a substring match for the search term
+# full name matches to the top
+# next, full email matches
+# next partial name matches
+# print list of names appended together
 
-term = input('Enter a search term: ')
+term = input('Enter a search term: ').lower()
 res = requests.get(f'http://127.0.0.1:8080/?search={term}')
-print('status code', res.status_code)
 results = res.json()
 print('results', json.dumps(results, indent=2))
 
 search_results = []
 
-# search whole name matches, if term found in a whole name, put in front
-# if name includes a space, include is results to be sorted
-# sort by highest to lowest, result of similar(name)    
-filtered_results = list(filter(lambda x: x['name'].find(' ') != -1, results))
-print('filtered results', filtered_results)
-for i, item in enumerate(filtered_results):
-    item['ratio'] = similar(item['name'], term)
-print('filtered results with ratio', filtered_results)
-sorted_results = sorted(filtered_results, key=lambda x: x['ratio'], reverse=True)
-print('sorted results', sorted_results)
+for result in results:
+    if term == result['name'].split()[0].lower() or term == result['name'].split()[1].lower():
+        search_results.append(result)
+        results.remove(result)
 
-# search email addresses, prioritize next
+for result in results:
+    if term in result['email'].lower():
+        search_results.append(result)
+        results.remove(result)
 
+for result in results:
+    if term in result['name'].lower():
+        search_results.append(result)
 
-# search incomplete names, append to the end of results
-
+print('search_results', json.dumps(search_results, indent=2))
